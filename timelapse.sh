a78d4d0a9ba75f824d6aa556a9592d8908b6d194
casu <casu@arcor.de>
2015-09-24 15:32:43 +0200

    Update timelapse.sh
    Changed to give options from command line.

--- timelapse.sh
+++ timelapse.sh
@@ -1,58 +1,66 @@
-!#/bin/bash
+#!/bin/bash
 ##################################################################
-# Author: Carsten Subbe 20150212                                 #
+# Author: Carsten Subbe 20150924                                 #
 # Description: Take all jpg Files within the actual directory    #
-# and create an avi video file. The images can be rotated before #
+# and create a avi video file. The images can be rotated before  #
 # video creation.                                                #
 # Dependencies: convert is part of the imagemagick package       #
 #               avconv has replaced ffmpeg nowadays              #
 #               sudo apt-get install imagemagick libav-tools     #
 ##################################################################
-echo "Create a timelapse video of all jpg files in this directory"
-echo ""
-echo "Do we have to rotate the source images?"
-echo "[0] no rotation"
-echo "[1] 90 degree"
-echo "[2] 180 degree"
-echo "[3] 270 degree"
-read answer
 
-case "$answer" in
-1) for file in `ls *.[jJ][pP][gG]`
-   do
-     convert -rotate 90 $file $file
-   done
-   ;;
-2) for file in `ls *.[jJ][pP][gG]`
-   do
-     convert -rotate 180 $file $file
-   done
-   ;;
-3) for file in `ls *.[jJ][pP][gG]`
-   do
-     convert -rotate 270 $file $file
-   done
-   ;;
-esac
+while getopts ":r:f:o:d" OPT
+do
+  case "$OPT" in
+    r ) rot=$OPTARG;;
+    f ) fps=$OPTARG;;
+    o ) filename=$OPTARG;;
+    \? ) help="1";;
+  esac
+done
+
+if [ "$help" == "1" -o "$#" == "0" ]
+then
+  echo ""
+  echo "Create a timelapse video of all jpg files in this directory"
+  echo "Usage: createtimelapse.sh [-h|-help] [-r 90|180|270] [-f fps] -o filename"
+  echo "-h|-help      : print this help"
+  echo "-r 90|180|270 : rotate images 90, 180 or 270 degrees"
+  echo "-f fps        : frames per second, range from 15-30 is usefull, default=20"
+  echo "-o filename   : name of the video file without extension"
+  echo "The original files will be copied to subfolder original/ before conversion."
+  echo ""
+  exit 1
+fi
+
+mkdir original
+if [ "$rot" != "" ]
+then
+  echo "Rotate all images $rot degree clockwise. Please be patient."
+  for file in `ls *.[jJ][pP][gG]`
+  do
+    cp $file original/
+    convert -rotate $rot $file $file
+  done
+fi
 
 echo "Now we are preparing the files."
 i=0
 for file in `ls *.[jJ][pP][gG]`
-do 
+do
   ln -s $file $(printf "%04d.jpg" $i)
   i=$((i+1));
 done
- 
-echo "Please enter the name of the output file (without extension)."
-read answer
-avconv -f image2 -r 25 -i ./%04d.jpg -c copy $answer.avi
-#avconv -f image2 -r 25 -i ./%04d.jpg -c copy -crf 20 $answer.avi
-echo "$answer.avi has been created"
+
+if [ "$fps" != "" ]
+then
+  avconv -f image2 -r $fps -i ./%04d.jpg -c copy $filename.avi
+else
+  avconv -f image2 -r 20 -i ./%04d.jpg -c copy $filename.avi
+fi
+echo "$filename has been created"
 find . -type l -name \*.jpg -exec unlink {} \;
 
-echo "Can we delete the jpg files (y/n)"
-read answer
-if [ "$answer" == "y" -o "$answer" == "Y" ]
-then
-  rm *.[jJ][pP][gG]
-fi
+rm *.[jJ][pP][gG]
+                            
+
